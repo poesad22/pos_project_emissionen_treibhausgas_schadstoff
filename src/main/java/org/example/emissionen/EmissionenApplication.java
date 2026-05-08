2dc81069a20ec6ff2b24dd010916a870753f9e41
poesad22 <poesad22@htl-kaindorf.at>
2026-05-08 11:12:53 +0200

    Initial commit

---
+++ src/main/java/org/example/emissionen/EmissionenApplication.java
@@ -0,0 +1,13 @@
+package org.example.emissionen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissionenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissionenApplication.class, args);
+	}
+
+}
