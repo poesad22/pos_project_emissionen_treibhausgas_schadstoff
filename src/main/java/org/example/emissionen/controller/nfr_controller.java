2bd7b28d93d6f8d94621605c4bd12e9537d7b3e0
poesad22 <poesad22@htl-kaindorf.at>
2026-05-08 11:17:32 +0200

    Added nfr controller

---
+++ src/main/java/org/example/emissionen/controller/nfr_controller.java
@@ -0,0 +1,9 @@
+package org.example.emissionen.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/nfr")
+public class nfr_controller {
+}
