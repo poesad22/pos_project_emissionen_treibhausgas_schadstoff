d12aec20196070bb3890928572ef80368f5556cc
poesad22 <poesad22@htl-kaindorf.at>
2026-05-11 09:10:28 +0200

    Write a service for Nfr data

---
+++ src/main/java/org/example/emissionen/service/NfrService.java
@@ -0,0 +1,18 @@
+package org.example.emissionen.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.emissionen.pojo.NfrItem;
+import org.example.emissionen.repository.NfrItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NfrService {
+    private NfrItemRepository nfrItemRepository;
+
+    public List<NfrItem> getNfrItems(){
+        return nfrItemRepository.findAll();
+    }
+}
