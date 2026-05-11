b4cb04c5cc23f11b2902356158bfc9134d9f677c
poesad22 <poesad22@htl-kaindorf.at>
2026-05-11 08:43:56 +0200

    Added Sektor annotations and variables

--- src/main/java/org/example/emissionen/pojo/Sektor.java
+++ src/main/java/org/example/emissionen/pojo/Sektor.java
@@ -1,4 +1,12 @@
 package org.example.emissionen.pojo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Sektor {
+    @Id
+    private Long sektorId;
+
+    private String sektorName;
 }
