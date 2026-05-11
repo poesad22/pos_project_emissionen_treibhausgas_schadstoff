3b6f93d6cd3a05d7ebffa306820601b63bca7d21
poesad22 <poesad22@htl-kaindorf.at>
2026-05-11 08:41:25 +0200

    Added nfr pojo fields from json

--- src/main/java/org/example/emissionen/pojo/NfrItem.java
+++ src/main/java/org/example/emissionen/pojo/NfrItem.java
@@ -3,8 +3,28 @@ package org.example.emissionen.pojo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
 @Entity
 public class NfrItem {
     @Id
     private Long NfrId;
+
+    private String region;
+
+    private String schadstoff;
+
+    private String einheit;
+
+    private int nfr_code;
+
+    private String nfr_sektor;
+
+    private String quelle;
+
+    private LocalDate datenstand;
+
+    private int jahr;
+
+    private float werte;
 }
