f9f75d9bb639e17164d985f872f6dc9cb075118f
poesad22 <poesad22@htl-kaindorf.at>
2026-05-12 14:28:34 +0200

    Added JsonProperty annotation for correct mapping by name

--- src/main/java/org/example/emissionen/pojo/NfrItem.java
+++ src/main/java/org/example/emissionen/pojo/NfrItem.java
@@ -2,6 +2,7 @@ package org.example.emissionen.pojo;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -27,6 +28,7 @@ public class NfrItem {
     @ManyToOne
     @JoinColumn(name = "sektorId")
     @JsonBackReference
+    @JsonProperty("nfr_sektor")
     private Sektor sektor;
 
     private String quelle;
