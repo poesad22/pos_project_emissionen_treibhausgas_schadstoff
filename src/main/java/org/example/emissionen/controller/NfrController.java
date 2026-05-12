1cc2e1ee670d35d3cef8264020db426e084df5a7
poesad22 <poesad22@htl-kaindorf.at>
2026-05-12 15:46:11 +0200

    Wrote /all get request

--- src/main/java/org/example/emissionen/controller/NfrController.java
+++ src/main/java/org/example/emissionen/controller/NfrController.java
@@ -1,13 +1,23 @@
 package org.example.emissionen.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.emissionen.pojo.NfrItem;
 import org.example.emissionen.repository.NfrItemRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/nfr")
 @RequiredArgsConstructor
 public class NfrController {
     private final NfrItemRepository nfrItemRepository;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<NfrItem>> getAll(){
+
+    }
 }
