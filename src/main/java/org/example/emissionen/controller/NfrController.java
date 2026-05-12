39d9f64ea402ccaa99970947b2052cb74a52fefb
poesad22 <poesad22@htl-kaindorf.at>
2026-05-12 15:45:26 +0200

    added nfr repository instance variable to controller

--- src/main/java/org/example/emissionen/controller/NfrController.java
+++ src/main/java/org/example/emissionen/controller/NfrController.java
@@ -1,9 +1,13 @@
 package org.example.emissionen.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.emissionen.repository.NfrItemRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/nfr")
+@RequiredArgsConstructor
 public class NfrController {
+    private final NfrItemRepository nfrItemRepository;
 }
