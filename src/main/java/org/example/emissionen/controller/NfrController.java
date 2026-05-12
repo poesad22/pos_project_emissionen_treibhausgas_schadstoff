4049f8ac8cf60caef5a91d5bb68a9b3c82063f57
poesad22 <poesad22@htl-kaindorf.at>
2026-05-12 15:46:33 +0200

    changed repository to use of a service

--- src/main/java/org/example/emissionen/controller/NfrController.java
+++ src/main/java/org/example/emissionen/controller/NfrController.java
@@ -3,6 +3,7 @@ package org.example.emissionen.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.emissionen.pojo.NfrItem;
 import org.example.emissionen.repository.NfrItemRepository;
+import org.example.emissionen.service.NfrService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import java.util.List;
 @RequestMapping("/api/nfr")
 @RequiredArgsConstructor
 public class NfrController {
-    private final NfrItemRepository nfrItemRepository;
+    private final NfrService nfrService;
 
     @GetMapping("/all")
     public ResponseEntity<List<NfrItem>> getAll(){
