78feab05755a5941c799c372dc56e079a21a3185
poesad22 <poesad22@htl-kaindorf.at>
2026-05-12 15:22:58 +0200

    Added auto generated ids

--- src/main/java/org/example/emissionen/pojo/NfrItem.java
+++ src/main/java/org/example/emissionen/pojo/NfrItem.java
@@ -3,10 +3,7 @@ package org.example.emissionen.pojo;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -15,6 +12,7 @@ import java.time.LocalDate;
 @Data
 public class NfrItem {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long NfrId;
 
     private String region;
--- src/main/java/org/example/emissionen/pojo/Sektor.java
+++ src/main/java/org/example/emissionen/pojo/Sektor.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Entity
 public class Sektor {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long sektorId;
 
     private String sektorName;
