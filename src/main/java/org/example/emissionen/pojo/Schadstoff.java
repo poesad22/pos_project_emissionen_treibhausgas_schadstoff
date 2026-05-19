6aeb3df07b1d62918fd00d149c48b4f1d07c3222
poesad22 <poesad22@htl-kaindorf.at>
2026-05-19 13:41:21 +0200

    Made Schadstoff an entity

--- src/main/java/org/example/emissionen/pojo/Schadstoff.java
+++ src/main/java/org/example/emissionen/pojo/Schadstoff.java
@@ -1,4 +1,15 @@
 package org.example.emissionen.pojo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Schadstoff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
