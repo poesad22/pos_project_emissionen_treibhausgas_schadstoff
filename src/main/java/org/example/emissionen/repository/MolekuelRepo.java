2a688807ac14b00a1cb4740a72e89a86dffbc6a2
poesad22 <poesad22@htl-kaindorf.at>
2026-05-19 15:34:23 +0200

    made naming upper case

--- src/main/java/org/example/emissionen/repository/MolekuelRepo.java
+++ src/main/java/org/example/emissionen/repository/MolekuelRepo.java
@@ -3,5 +3,5 @@ package org.example.emissionen.repository;
 import org.example.emissionen.pojo.Molekuel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface molekuelRepo extends JpaRepository<Molekuel, String> {
+public interface MolekuelRepo extends JpaRepository<Molekuel, String> {
 }
