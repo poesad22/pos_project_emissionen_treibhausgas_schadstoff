ed3be6ea841a835f761d4ecf8f616c296982170f
poesad22 <poesad22@htl-kaindorf.at>
2026-05-19 15:34:43 +0200

    added repo to service for molekuel

--- src/main/java/org/example/emissionen/service/MolekuelService.java
+++ src/main/java/org/example/emissionen/service/MolekuelService.java
@@ -1,7 +1,11 @@
 package org.example.emissionen.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.emissionen.repository.MolekuelRepo;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MolekuelService {
+    private final MolekuelRepo molekuelRepo;
 }
