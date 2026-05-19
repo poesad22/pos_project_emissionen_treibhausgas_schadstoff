64874a7410ca1db5c4cce1020998f3602ef20f3f
poesad22 <poesad22@htl-kaindorf.at>
2026-05-19 15:47:04 +0200

    created getSiedepunkt for a moleukuel

--- src/main/java/org/example/emissionen/service/MolekuelService.java
+++ src/main/java/org/example/emissionen/service/MolekuelService.java
@@ -1,6 +1,8 @@
 package org.example.emissionen.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.emissionen.pojo.Atom;
+import org.example.emissionen.pojo.Molekuel;
 import org.example.emissionen.repository.AtomRepo;
 import org.example.emissionen.repository.MolekuelRepo;
 import org.springframework.stereotype.Service;
@@ -10,5 +12,13 @@ import org.springframework.stereotype.Service;
 public class MolekuelService {
     private final MolekuelRepo molekuelRepo;
     private final AtomRepo atomRepo;
+
+    public double getSiedepunkt(Molekuel molekuel) {
+        return molekuel.getAtome().stream()
+                .map(Atom::getBoil)
+                .filter(b -> b != null)
+                .max(Double::compareTo)
+                .orElse(0.0);
+    }
 
 }
